3ac1a8805ffd660a4cd1ee2b152c1e3a2cb76681
Roury Hinds <roury_hinds@outlook.com>
2020-12-15 13:04:22 +0000

    oystard class created, responds to top_up method - tests passing and refactored

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
-describe Oystcard do
+require 'oystercard'
+# rspec ./spec/oystercard_spec.rb
 
+describe Oystercard do
+  it 'balance defaults to zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  describe '#top_up' do
+    it 'can increase balance' do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
 end
